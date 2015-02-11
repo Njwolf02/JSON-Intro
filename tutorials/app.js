2d35e7c72d6b43cb56c971145caa51421eed638f
Nick Wolf <njwolf64@gmail.com>
2015-02-11 16:45:11 -0500

    test table 2

--- tutorials/app.js
+++ tutorials/app.js
@@ -9,13 +9,21 @@
     list.tutorials = data;
   });
 }]);
-  app.controller('ReviewController', function() {
-    this.review = {};
-
-    this.addReview = function(list) {
-      list.reviews.push(this.review);
-
-      this.review = {};
-    };
-  });
+  
+	  app.controller('ExampleController', ['$scope', '$http', '$filter', function($scope, $http,$filter) {
+		var orderBy = $filter('orderBy');
+		$scope.tutorials = [];
+		
+		
+		$http.get('tutorials.json').success(function(data) {
+			$scope.items = data;
+		});
+		
+		$scope.order = function(predicate, reverse) {
+		  $scope.tutorials = orderBy($scope.tutorials, predicate, reverse);
+		};
+		
+		
+		$scope.order('-author',false);
+	  }]);
 })();
